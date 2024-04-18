47fa8d0624a7db0c4f33a56d438a00dc1727995e
manasdeo <rmnslogin@gmail.com>
2024-04-18 09:17:34 +0530

    Search users

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -2,52 +2,39 @@ import { type Request, type Response } from "express";
 import axios from "axios";
 import User from "../models/Users.models";
 
+interface SearchQuery {
+  [key: string]: RegExp | number | boolean | undefined;
+}
+
 const GIT_URL = "https://api.github.com/users";
 
 const testController = async (req: Request, res: Response) => {
-  const { username } = req.params;
+  const { ...queries } = req.query;
 
   try {
-    const user = await User.findOne({
-      username: new RegExp(`^${username}$`, "i"),
+    const searchQuery: SearchQuery = {};
+
+    Object.keys(queries).forEach((param) => {
+      if (!isNaN(Number(queries[param]))) {
+        searchQuery[param] = Number(queries[param]);
+      } else if (typeof queries[param] === "boolean") {
+        searchQuery[param] = queries[param] === "true";
+      } else {
+        searchQuery[param] = new RegExp(queries[param] as string, "i");
+      }
     });
 
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
+    const users = await User.find({ isDeleted: false, ...searchQuery });
+
+    if (users.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No users found matching the search criteria" });
     }
 
-    const followingResponse = await axios.get(
-      `${GIT_URL}/${username}/following`
-    ); // Fetch the user's following lists from the GitHub API
-    const followersResponse = await axios.get(
-      `${GIT_URL}/${username}/followers`
-    ); // Fetch the user's followers lists from the GitHub API
-
-    const following = followingResponse.data.map(
-      (followee: { login: string }) => followee.login
-    );
-    const followers = followersResponse.data.map(
-      (follower: { login: string }) => follower.login
-    );
-
-    const mutualFollowers = followers.filter((follower: { login: string }) =>
-      following.includes(follower)
-    ); // Find mutual followers (users the given user follows who also follow them back)
-
-    const updatedUser = await User.findOneAndUpdate(
-      { username: new RegExp(`^${username}$`, "i") },
-      {
-        $set: { friends: mutualFollowers },
-      },
-      { new: true }
-    );
-
-    res.status(200).json({
-      message: "Mutual followers saved as friends",
-      user: updatedUser,
-    });
+    res.status(200).json({ message: "Users found!", users });
   } catch (error) {
-    res.status(500).json({ message: "Failed to find mutual followers", error });
+    res.status(500).json({ message: "Error searching for users", error });
   }
 };
 
@@ -125,4 +112,34 @@ const findMutualFollowers = async (req: Request, res: Response) => {
   }
 };
 
-export { testController, saveUser, findMutualFollowers };
+const searchUsers = async (req: Request, res: Response) => {
+  const { ...queries } = req.query;
+
+  try {
+    const searchQuery: SearchQuery = {};
+
+    Object.keys(queries).forEach((param) => {
+      if (!isNaN(Number(queries[param]))) {
+        searchQuery[param] = Number(queries[param]);
+      } else if (typeof queries[param] === "boolean") {
+        searchQuery[param] = queries[param] === "true";
+      } else {
+        searchQuery[param] = new RegExp(queries[param] as string, "i");
+      }
+    });
+
+    const users = await User.find({ isDeleted: false, ...searchQuery });
+
+    if (users.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "No users found matching the search criteria" });
+    }
+
+    res.status(200).json({ message: "Users found!", users });
+  } catch (error) {
+    res.status(500).json({ message: "Error searching for users", error });
+  }
+};
+
+export { testController, saveUser, findMutualFollowers, searchUsers };
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -3,14 +3,17 @@ import {
   testController,
   saveUser,
   findMutualFollowers,
+  searchUsers,
 } from "../controllers/user.controller";
 
 const router = express.Router();
 
-router.get("/test/:username", testController); // Test Controller
+router.get("/test", testController); // Test Controller
 
 router.get("/save-user/:username", saveUser); // GitHub User Data Storage
 
 router.get("/find-mutual-followers/:username", findMutualFollowers); // Mutual Followers as Friends
+
+router.get("/search-users", searchUsers); // Search Functionality
 
 export default router;
