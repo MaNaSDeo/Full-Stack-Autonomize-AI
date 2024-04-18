f96d2738c729324e0d958c48566ce6bf732e61eb
manasdeo <rmnslogin@gmail.com>
2024-04-18 14:09:14 +0530

    main package json setup

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -100,7 +100,7 @@ const searchUsers = async (req: Request, res: Response) => {
     });
 
     const page = Number(req.query.page) || 1;
-    const limit = Number(req.query.limit) || 2;
+    const limit = Number(req.query.limit) || 10;
     const skip = (page - 1) * limit;
 
     const totalUsers = await User.countDocuments({
