a35a9f629a07ecdbfe0d3076a26308a760f89d55
manasdeo <rmnslogin@gmail.com>
2024-04-19 14:28:53 +0530

    RepoCard with static data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import styles from "./App.module.scss";
 import InputPage from "./Components/InputPage";
 import Header from "./Components/Header";
+import RepoCard from "./Components/RepoCard";
 
 import { useSelector } from "react-redux";
 import { InputState } from "./store/types";
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <h1>{inputText}</h1>
       <InputPage />
+      <RepoCard />
     </div>
   );
 }
