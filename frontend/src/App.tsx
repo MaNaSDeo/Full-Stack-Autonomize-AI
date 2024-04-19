9373522c5134693b0e13496470875d3aee0c3703
manasdeo <rmnslogin@gmail.com>
2024-04-19 23:56:05 +0530

    .

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
     <div className={styles.app}>
       <Header />
       <InputPage />
-      {/* <RepoContainerPage /> */}
+      <RepoContainerPage />
       <RepoDetail />
     </div>
   );
